ec5c9977eae34804a34328c74d0c18758b771314
MrT <dzho-tazjukov-ilnaz@qa.studio>
2023-02-02 17:55:22 +0400

    Initial commit

---
+++ js.js
@@ -0,0 +1,4 @@
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+	alert('Тестирование программ может показать наличие ошибок, но никогда не покажет их отсутствие.')
+})
